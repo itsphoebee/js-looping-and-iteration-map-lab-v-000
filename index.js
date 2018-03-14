fdee640b558fc1e1912cc41fb6d6dadbc2f7dcd1
Phoebe Lau <phoebelau7@gmail.com>
2018-03-14 23:03:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ function lowerCaseDrivers(drivers) {
 
 function nameToAttributes(drivers) {
   return drivers.map(function (name) {
-    return {firstName: name.split(' ')[0], lastName: name.split(' ')[1]};
+    return {firstName = name.split(' ')[0], lastName = name.split(' ')[1]};
   });
 }
+
 
 function attributesToPhrase(drivers) {
   return drivers.map(driver => `${driver.name} is from ${driver.hometown}`)
