eda754f555478fbaebca4343e1a4e26e732a0dc7
Phoebe Lau <phoebelau7@gmail.com>
2018-03-14 22:54:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function nameToAttributes(drivers) {
 }
 
 function attributesToPhrase(drivers) {
-  return drivers.map(driver => return `${driver.name} is from ${driver.hometown}`)
+  return drivers.map(driver => `${driver.name} is from ${driver.hometown}`)
 
 }
