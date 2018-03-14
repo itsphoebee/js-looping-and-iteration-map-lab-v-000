5331fad789aeb11f0beebebeca09426e85fb4658
Phoebe Lau <phoebelau7@gmail.com>
2018-03-14 22:47:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function lowerCaseDrivers(drivers) {
 }
 
 function nameToAttributes(drivers) {
-  return drivers.map(function (driver) {
-    const driverFirst = driver.split(' ')[0];
-    const driverLast = driver.split(' ')[1];
+  return drivers.map(function (name) {
+    const firstName = name.split(' ')[0];
+    const lastName = name.split(' ')[1];
 
     return { firstName: driverFirst, lastName: driverLast };
-});
+  });
 }
